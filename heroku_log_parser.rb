a9cb7371b481052902dde8eacb144e28bdd21761
yottaflops <yottaflops89@gmail.com>
2014-11-26 15:26:58 -0500

    calculates metrics per url

--- heroku_log_parser.rb
+++ heroku_log_parser.rb
@@ -1,101 +1,145 @@
 # Run via terminal: ruby heroku_log_parser.rb [LOG_PATH]
 
+require 'pry'
 class HerokuLogParser
   def initialize(path)
     raise FileNotFoundError, "There is no file with that name in the specified directory" unless File.exists?(path)
 
     @log = File.open(path)
-    @dynos = []
-    @response_times = []
-    @endpoints = { pending_msgs: 0, messages: 0, get_f_progress: 0, get_f_score: 0, user_post: 0, user_get: 0 }
+    @endpoints = {
+      pending_msgs: { requests: 0, dynos: [], response_times: [] },
+      messages: { requests: 0, dynos: [], response_times: [] },
+      get_f_progress: { requests: 0, dynos: [], response_times: [] },
+      get_f_score: { requests: 0, dynos: [], response_times: [] },
+      user_post: { requests: 0, dynos: [], response_times: [] },
+      user_get: { requests: 0, dynos: [], response_times: [] },
+      other: { requests: 0, dynos: [], response_times: [] }
+    }
     @total_requests = 0
   end
 
   def parseLog
     @log.each_line do |line|
-      cache_dyno(line)
-      cache_response_time(line)
-      tally_request(line)
+      request_key = get_request_key(line)
+      tally_request(request_key)
+      cache_dyno(line, request_key)
+      cache_response_time(line, request_key)
       @total_requests += 1
     end
-
+    puts @endpoints
     pretty_print_outputs
   end
 
  private
 
-  def cache_dyno(text)
-    dyno_param = /dyno\S*/.match(text).to_s
-    dyno = dyno_param.match(/\d+/)[0].to_i
-
-    @dynos.push(dyno)
-  end
-
-  def cache_response_time(text)
-    response_param = /connect\S*/.match(text).to_s
-    response_time = response_param.match(/\d+/)[0].to_i
-
-    @response_times.push(response_time)
-  end
-
-  def tally_request(text)
+  def get_request_key(text)
     method_param = /method\S*/.match(text).to_s
     request_method = method_param.match(/[^method=].*/)[0].to_s
-
-    return @endpoints[:user_post] += 1 if request_method == "POST"
-
     path_param = /path\S*/.match(text).to_s
     request_path = path_param.match(/[^path=].*/)[0].to_s
 
-    if request_path =~ /count_pending_messages/
-      @endpoints[:pending_msgs] += 1
-    elsif request_path =~ /get_messages/
-      @endpoints[:messages] += 1
-    elsif request_path =~ /get_friends_progress/
-      @endpoints[:get_f_progress] += 1
-    elsif request_path =~ /get_friends_score/
-      @endpoints[:get_f_score] += 1
+    if request_method == "POST"
+      if request_path =~ /users/
+        return :user_post
+      else
+        return :other
+      end
+
+    elsif request_method == "GET"
+      if request_path =~ /count_pending_messages/
+        :pending_msgs
+      elsif request_path =~ /get_messages/
+        :messages
+      elsif request_path =~ /get_friends_progress/
+        :get_f_progress
+      elsif request_path =~ /get_friends_score/
+        :get_f_score
+      elsif request_path =~ /users/
+        :user_get
+      else
+        :other
+      end
     else
-      @endpoints[:user_get] += 1
+      return :other
     end
   end
 
-  def output_response_times
-    puts "The mean response time was #{@response_times.mean} ms."
-    puts "The median response time was #{@response_times.median} ms."
-    puts "The mode of the response times was #{@response_times.mode} ms."
+  def cache_dyno(text, request_key)
+    dyno_param = /dyno\S*/.match(text).to_s
+    dyno = dyno_param.match(/\d+/)[0].to_i
+
+    @endpoints[request_key][:dynos].push(dyno)
   end
 
-  def output_request_metrics
-    puts "Out of #{@total_requests} requests:"
-    puts ""
-    puts "There were #{@endpoints[:pending_msgs]} GET requests to '/api/users/{user_id}/count_pending_messages'."
-    puts "There were #{@endpoints[:messages]} GET requests to '/api/users/{user_id}/get_messages'."
-    puts "There were #{@endpoints[:get_f_progress]} GET requests to '/api/users/{user_id}/get_friends_progress'."
-    puts "There were #{@endpoints[:get_f_score]} GET requests to '/api/users/{user_id}/get_friends_score'."
-    puts "There were #{@endpoints[:user_get]} GET requests to '/api/users/{user_id}'."
-    puts "There were #{@endpoints[:user_post]} POST requests to '/api/users/{user_id}'."
+  def cache_response_time(text, request_key)
+    connect_param = /connect\S*/.match(text).to_s
+    connect_time = connect_param.match(/\d+/)[0].to_i
+
+    service_param = /service=\S*/.match(text).to_s
+    service_time = service_param.match(/\d+/)[0].to_i
+
+    response_time = connect_time + service_time
+
+    @endpoints[request_key][:response_times].push(response_time)
   end
 
-  def output_dyno_metrics
-    puts "The most active dyno was dyno ##{@dynos.mode}."
-    puts "The least active dyno was dyno ##{@dynos.mode(true)}."
+  def tally_request(request_key)
+    @endpoints[request_key][:requests] += 1
+  end
+
+  def output_request_metrics(request_key)
+    output_request_totals(request_key)
+    output_response_times(request_key)
+    output_dyno_metrics(request_key)
+  end
+
+  def output_request_totals(request_key)
+    puts "Requests made: #{@endpoints[request_key][:requests]}"
+  end
+
+  def output_response_times(request_key)
+    puts "Mean response time: #{@endpoints[request_key][:response_times].mean} ms"
+    puts "Median response time: #{@endpoints[request_key][:response_times].median} ms"
+    puts "Mode of response times: #{@endpoints[request_key][:response_times].mode} ms"
+  end
+
+  def output_dyno_metrics(request_key)
+    puts "Most active dyno: #{@endpoints[request_key][:dynos].mode}"
+    puts "Least active dyno: #{@endpoints[request_key][:dynos].mode(true)}"
   end
 
   def pretty_print_outputs
     puts""
-    puts "\e[36mREQUEST METRICS"
-    puts "===============\033[0m\n"
-    output_request_metrics
-    puts""
-    puts "\e[36mRESPONSE TIME METRICS"
-    puts "=====================\033[0m\n"
-    output_response_times
-    puts""
-    puts "\e[36mDYNO USAGE METRICS"
-    puts "==================\033[0m\n"
-    output_dyno_metrics
-    puts""
+    puts "Out of #{@total_requests} requests:"
+    puts ""
+    puts "\e[36mGET requests to '/api/users/{user_id}/count_pending_messages'"
+    puts "=============================================================\033[0m\n"
+    output_request_metrics(:pending_msgs)
+    puts ""
+    puts "\e[36mGET requests to '/api/users/{user_id}/get_messages'"
+    puts "===================================================\033[0m\n"
+    output_request_metrics(:messages)
+    puts ""
+    puts "\e[36mGET requests to '/api/users/{user_id}/get_friends_progress'"
+    puts "===========================================================\033[0m\n"
+    output_request_metrics(:get_f_progress)
+    puts ""
+    puts "\e[36mGET requests to '/api/users/{user_id}/get_friends_score'"
+    puts "========================================================\033[0m\n"
+    output_request_metrics(:get_f_score)
+    puts ""
+    puts "\e[36mGET requests to '/api/users/{user_id}'"
+    puts "======================================\033[0m\n"
+    output_request_metrics(:user_get)
+    puts ""
+    puts "\e[36mPOST requests to '/api/users/{user_id}'"
+    puts "=======================================\033[0m\n"
+    output_request_metrics(:user_post)
+    puts ""
+    puts "\e[36mPOST requests to other urls"
+    puts "=======================================\033[0m\n"
+    output_request_metrics(:other)
+    puts ""
   end
 end
 
